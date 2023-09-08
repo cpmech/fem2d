dc44736f5acdc6123dcff1e5a3f4989f19bfab09
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-09-08 11:36:24 +1000

    Add plane-strain test

--- src/lib/z_test_solid2d.cpp
+++ src/lib/z_test_solid2d.cpp
@@ -150,7 +150,7 @@ TEST_CASE("solid2d") {
 
         // solve the linear system
         fem->solve();
-        print_vector("uu", fem->uu);
+        // print_vector("uu", fem->uu);
 
         // check solution
         auto correct_uu = vector<double>{
@@ -160,6 +160,114 @@ TEST_CASE("solid2d") {
             4.727650463081949e-03, -2.473565538172127e-02,  // 3
             -1.313941349422282e-02, -5.549310752960183e-02, // 4
             8.389015766816341e-05, -5.556637423271112e-02}; // 5
+        CHECK(equal_vectors_tol(fem->uu, correct_uu, 1e-15));
+    }
+
+    SUBCASE("plane-strain domain (Smith's Example 5.2)") {
+        // Smith IM, Griffiths DV, and Margetts L (2014) Programming the Finite
+        // Element Method, Wiley, Fifth Edition, 664p
+        //
+        // TEST GOAL
+        //
+        // This test verifies a plane-strain simulation with Tri3 elements
+        //
+        // MESH
+        //
+        //               1.0 kN/m²
+        //         ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+        //  0.0  ▷0---------1---------2
+        //         |       ,'|       ,'|   E = 1e6 kN/m²
+        //         |  0  ,'  |  2  ,'  |   ν = 0.3
+        //         |   ,'    |   ,'    |
+        //         | ,'   1  | ,'  3   |   connectivity:
+        // -0.5  ▷3'--------4'--------5     0 : 1 0 3
+        //         |       ,'|       ,'|     1 : 3 4 1
+        //         |  4  ,'  |  6  ,'  |     2 : 2 1 4
+        //         |   ,'    |   ,'    |     3 : 4 5 2
+        //         | ,'   5  | ,'   7  |     4 : 4 3 6
+        // -1.0  ▷6'--------7'--------8     5 : 6 7 4
+        //         △        △        △    6 : 5 4 7
+        //                                   7 : 7 8 5
+        //        0.0       0.5       1.0
+        //
+        // BOUNDARY CONDITIONS
+        //
+        // Fix left edge horizontally
+        // Fix bottom edge vertically
+        // Distributed load Qn = -1.0 on top edge
+        //
+        // CONFIGURATION AND PARAMETERS
+        //
+        // Static simulation
+        // Young = 1e6
+        // Poisson = 0.3
+        // Plane-strain
+
+        // input data
+        auto solid_triangle = true;
+        auto plane_stress = false;
+        auto thickness = 1.0;
+        auto coordinates = vector<double>{
+            0.0, 0.0,   // 0
+            0.5, 0.0,   // 1
+            1.0, 0.0,   // 2
+            0.0, -0.5,  // 3
+            0.5, -0.5,  // 4
+            1.0, -0.5,  // 5
+            0.0, -1.0,  // 6
+            0.5, -1.0,  // 7
+            1.0, -1.0}; // 8
+        auto connectivity = vector<size_t>{
+            1, 0, 3,  // 0
+            3, 4, 1,  // 1
+            2, 1, 4,  // 2
+            4, 5, 2,  // 3
+            4, 3, 6,  // 4
+            6, 7, 4,  // 5
+            5, 4, 7,  // 6
+            7, 8, 5}; // 7
+        auto param_young = vector<double>(8, 1e6);
+        auto param_poisson = vector<double>(8, 0.3);
+        auto param_cross_area = vector<double>{};
+        map<node_dof_pair_t, double> essential_bcs{
+            {{0, AlongX}, 0.0},
+            {{3, AlongX}, 0.0},
+            {{6, AlongX}, 0.0},
+            {{6, AlongY}, 0.0},
+            {{7, AlongY}, 0.0},
+            {{8, AlongY}, 0.0}};
+        map<node_dof_pair_t, double> natural_bcs{
+            {{0, AlongY}, -0.25},
+            {{1, AlongY}, -0.50},
+            {{2, AlongY}, -0.25}};
+
+        // allocate fem solver
+        auto fem = Fem2d::make_new(solid_triangle,
+                                   plane_stress,
+                                   thickness,
+                                   coordinates,
+                                   connectivity,
+                                   param_young,
+                                   param_poisson,
+                                   param_cross_area,
+                                   essential_bcs,
+                                   natural_bcs);
+
+        // solve the linear system
+        fem->solve();
+        // print_vector("uu", fem->uu);
+
+        // check solution
+        auto correct_uu = vector<double>{
+            0.000000000000000e+00, -9.100000000000005e-07, // 0
+            1.950000000000001e-07, -9.100000000000006e-07, // 1
+            3.900000000000002e-07, -9.100000000000000e-07, // 2
+            0.000000000000000e+00, -4.550000000000002e-07, // 3
+            1.950000000000002e-07, -4.550000000000004e-07, // 4
+            3.900000000000004e-07, -4.549999999999999e-07, // 5
+            0.000000000000000e+00, 0.000000000000000e+00,  // 6
+            1.950000000000004e-07, 0.000000000000000e+00,  // 7
+            3.900000000000004e-07, 0.000000000000000e+00}; // 8
         CHECK(equal_vectors_tol(fem->uu, correct_uu, 1e-15));
     }
 }
